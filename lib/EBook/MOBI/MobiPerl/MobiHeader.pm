8a1401d1948ed2b090a51b743984151b60f8eee1
Boris Däppen <bdaeppen.perl@gmail.com>
2013-01-14 14:43:07 +0000

    Import of BORISD/EBook-MOBI-0.56 from CPAN.
    gitpan-cpan-distribution: EBook-MOBI
    gitpan-cpan-version:      0.56
    gitpan-cpan-path:         BORISD/EBook-MOBI-0.56.tar.gz
    gitpan-cpan-author:       BORISD
    gitpan-cpan-maturity:     released

--- lib/EBook/MOBI/MobiPerl/MobiHeader.pm
+++ lib/EBook/MOBI/MobiPerl/MobiHeader.pm
@@ -79,7 +79,7 @@ use strict;
 #
 # TYPE: 2=book
 #
-our $VERSION = '0.55'; # VERSION: Should be 3 or 4
+our $VERSION = '0.56'; # VERSION: Should be 3 or 4
 #
 # CODEPAGE: utf-8: 65001; westerner: 1252
 #
