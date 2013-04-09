aca15c1c7789605ecd12a51d1fcebf48620bfb07
Boris Däppen <bdaeppen.perl@gmail.com>
2013-04-09 12:46:42 +0000

    Import of BORISD/EBook-MOBI-0.581 from CPAN.
    gitpan-cpan-distribution: EBook-MOBI
    gitpan-cpan-version:      0.581
    gitpan-cpan-path:         BORISD/EBook-MOBI-0.581.tar.gz
    gitpan-cpan-author:       BORISD
    gitpan-cpan-maturity:     released

--- lib/EBook/MOBI/Driver/Example.pm
+++ lib/EBook/MOBI/Driver/Example.pm
@@ -1,6 +1,6 @@
 package EBook::MOBI::Driver::Example;
 
-our $VERSION = '0.58'; # VERSION (hook for Dist::Zilla::Plugin::OurPkgVersion)
+our $VERSION = '0.581'; # VERSION (hook for Dist::Zilla::Plugin::OurPkgVersion)
 
 use feature 'switch';
 
