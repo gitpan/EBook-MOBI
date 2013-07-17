b991d9d8ebdd2d20778167865e6f404f249a70bd
Boris Däppen <bdaeppen.perl@gmail.com>
2013-07-17 20:52:00 +0000

    Import of BORISD/EBook-MOBI-0.65 from CPAN.
    gitpan-cpan-distribution: EBook-MOBI
    gitpan-cpan-version:      0.65
    gitpan-cpan-path:         BORISD/EBook-MOBI-0.65.tar.gz
    gitpan-cpan-author:       BORISD
    gitpan-cpan-maturity:     released

--- lib/EBook/MOBI/Driver.pm
+++ lib/EBook/MOBI/Driver.pm
@@ -1,6 +1,6 @@
 package EBook::MOBI::Driver;
 
-our $VERSION = '0.65'; # TRIAL VERSION (hook for Dist::Zilla::Plugin::OurPkgVersion)
+our $VERSION = '0.65'; # VERSION (hook for Dist::Zilla::Plugin::OurPkgVersion)
 
 use strict;
 use warnings;
