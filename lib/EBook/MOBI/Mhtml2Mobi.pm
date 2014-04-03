fb43f53cf476b21f4c0ece4f20f9e295554219ea
Boris Däppen <bdaeppen.perl@gmail.com>
2014-04-03 19:19:58 +0000

    Import of BORISD/EBook-MOBI-0.69 from CPAN.
    gitpan-cpan-distribution: EBook-MOBI
    gitpan-cpan-version:      0.69
    gitpan-cpan-path:         BORISD/EBook-MOBI-0.69.tar.gz
    gitpan-cpan-author:       BORISD
    gitpan-cpan-maturity:     released

--- lib/EBook/MOBI/Mhtml2Mobi.pm
+++ lib/EBook/MOBI/Mhtml2Mobi.pm
@@ -1,6 +1,6 @@
 package EBook::MOBI::Mhtml2Mobi;
 
-our $VERSION = '0.69'; # TRIAL VERSION (hook for Dist::Zilla::Plugin::OurPkgVersion)
+our $VERSION = '0.69'; # VERSION (hook for Dist::Zilla::Plugin::OurPkgVersion)
 
 # This file contains some example code, borrowed from MobiPerl.
 # The code comes from the html2mobi file from MobiPerl.
